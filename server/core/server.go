3b1882a7429e240e6903f43b0488b0f0d2d81dc0
zhaol <373900859@qq.com>
2025-09-18 11:39:33 +0800

    base router ready

--- server/core/server.go
+++ server/core/server.go
@@ -16,6 +16,10 @@ func RunServer() {
 	addr := global.Config.System.Addr()
 	Router := initialize.InitRouter()
 
+	//routes := Router.Routes()
+	//for _, route := range routes {
+	//	global.Log.Info("已注册路由", zap.String("method", route.Method), zap.String("path", route.Path))
+	//}
 	// 启动服务器
 	s := initServer(addr, Router)
 	global.Log.Info("服务器启动成功", zap.String("监听地址", addr))
--- server/initialize/router.go
+++ server/initialize/router.go
@@ -2,7 +2,10 @@ package initialize
 
 import (
 	"server/global"
+	"server/router"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,7 +14,16 @@ func InitRouter() *gin.Engine {
 	gin.SetMode(global.Config.System.Env)
 	Router := gin.Default()
 
-	// TODO: 路由分组
+	var store = cookie.NewStore([]byte(global.Config.System.SessionsSecret))
+	Router.Use(sessions.Sessions("session", store))
+
+	// 路由分组
+	routerGroup := router.RouterGroupApp
+
+	publicGroup := Router.Group(global.Config.System.RouterPrefix)
+	{
+		routerGroup.InitBaseRouter(publicGroup)
+	}
 
 	return Router
 }
